c7ec375bcdb3b7103001b5e14c249159bd4a4d09
LeJacque Gillespie <m0321us@yahoo.com>
2016-07-06 21:18:59 -0500

    Initial commit

---
+++ app/src/main/java/g620apps/profilepage/ProfileScreenMainActivity.java
@@ -0,0 +1,17 @@
+package g620apps.profilepage;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class ProfileScreenMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_screen_main);
+    }
+}
+
+//add propler photos to mipmap folder and xml file
+//adjust the strings folder with proper string resources
---
+++ app/src/main/java/g620apps/profilepage/R.java
@@ -0,0 +1,7 @@
+package g620apps.profilepage;
+
+/**
+ * Created by yuriana on 06/07/2016.
+ */
+public class R {
+}
